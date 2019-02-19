be1d38eafd9487c67e7b95c82500f4932de745ae
Roksana <k1327385@kcl.ac.uk>
2019-02-19 10:23:32 +0000

    adding Flask app file and initial database

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 18 14:44:14 2019
+from flask import Flask, render_template, request
+from classes_db import *
 
-@author: hienh
-"""
+app = Flask(__name__)
 
-from flask import Flask,render_template,request
-from flask_
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/taskadded", methods = ["POST"])
+def add_task():
+    form_data = request.form
+    task = form_data["task_title"]
+    task_desc = form_data["task_desc"]
+    result = request.form.getlist("checkbox")
+    print(result)
+    result2= "".join(result)
+    print (result2)
+    if result2 == "checked":
+        importance = "yes"
+    else:
+        importance = "no"
+    return render_template("index.html", **locals()) 
+    # important = form_data["extras"]
+    # if important == "checked":
+    #     importance = "yes"
+    # else:
+    #     importance = "no"
+    #    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

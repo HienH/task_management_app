6ff9a9e2d9372c923d307f1a830aa15e2f04c2f2
Roksana <k1327385@kcl.ac.uk>
2019-02-19 13:16:20 +0000

    adding database creation and TEST file

--- app.py
+++ app.py
@@ -1,47 +1,17 @@
+
+from database import *
 from flask import Flask, render_template, request
-from classes_db import *
-
+# from classes_db import *
 app = Flask(__name__)
 
 
-app = Flask(__name__)
-cors = CORS(app)
+#---------Connect Flask to database----------------#
+app.config['SQLALCHEMY_DATABASE URI'] = 'sqlite:////c/Users/RN/FINAL_PROJECTS/task_management_app/taskdatabase.db'
 
-todos = [
-        { "id": 0,
-         "title": "First Task",
-         "description": "this is the first description",
-         "important": 0,
-         "status": 0,
-         "date": "01-01-2019"
-         }, {
-         "id": 1,
-         "title": "First Task",
-         "description": "this is the first description",
-         "important": 0,
-         "status": 0,
-         "date": "03-02-2019"},
-          {
-         "id": 2,
-         "title": "third Task",
-         "description": "this is the third description",
-         "important": 1,
-         "status": 1,
-         "date": "02-02-2019"},
-        ]
+# db = sqlalchemy(app)
 
-@app.route('/todos',methods = ['GET'])
-def alltodos():
-    return jsonify(todos)
 
-#GET INDIVIUDAL TASK API
-@app.route('/todos/<int:id>',methods = ['GET'])
-def get_todo(id):
-    for todo in todos:
-        if(id == todo["id"]):
-            return jsonify(todo),200
-    return "Todo not found",404
-       
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -60,14 +30,14 @@ def add_task():
     else:
         importance = "no"
     return render_template("index.html", **locals()) 
-    # important = form_data["extras"]
-    # if important == "checked":
-    #     importance = "yes"
-    # else:
-    #     importance = "no"
-    #    
-
+    important = form_data["extras"]
+    if important == "checked":
+        importance = "yes"
+    else:
+        importance = "no"
+       
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
